c245c234719852abbba5c5f799184c8cbba4d2b0
Ananya Rajput <ananyaarajput2003@gmail.com>
2026-07-30 10:39:53 +0530

    Time: 0 ms (100%), Space: 42.6 MB (99.04%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,17 +1,12 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
-        for(int i=0;i<word.length();i++){
-            freq[word.charAt(i)-'a']++;
+        int n = word.length();
+        int pushCnt = 0;
+
+        for(int i = 0; i < n; i++){
+            pushCnt += i / 8 + 1;
         }
-        Arrays.sort(freq);
-        int totalpushes=0;
-        for(int i=25;i>=0;i--){
-            if(freq[i]==0) break;
-            int rank = 25-i;
-            int push = (rank/8)+1;
-            totalpushes+=freq[i]*push;
-        }
-        return totalpushes;
+
+        return pushCnt;
     }
 }
