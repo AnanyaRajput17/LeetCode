17a64ba2d4c96e3f868a0d57d4d005603750594d
Ananya Rajput <ananyaarajput2003@gmail.com>
2026-08-31 10:53:28 +0530

    Time: 7 ms (65.78%), Space: 46.8 MB (26.78%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,18 +1,32 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        return func(nums,target ,n-1 );
+        int sum = 0;
+        for(int x: nums){
+            sum+=x;
+        }
+        if(Math.abs(target)>sum) return 0;
+        int offset = sum;
+        int[][]  dp = new int[n][2*offset+1];
+        for(int[] rows: dp){
+            
+                Arrays.fill(rows,-1);
+            
+        }
+        return func(nums,dp,target ,n-1 ,offset);
         
     }
-    public int func(int[]  nums , int target , int index){
+    public int func(int[]  nums , int[][] dp ,int target , int index,int offset){
+        if(target > offset || target < -offset ) return 0;
         if(index==0){
             if(nums[index]==0&& target == 0) return 2;
             if(nums[index] == Math.abs(target))
             return 1;
             else return 0;
         }
-        int add = func(nums,target - nums[index],index-1);
-        int sub = func(nums , target + nums[index],index-1 );
-        return add + sub;
+        if(dp[index][target+offset]!=-1)return dp[index][target+offset];
+        int add = func(nums,dp,target - nums[index],index-1 , offset);
+        int sub = func(nums ,dp, target + nums[index],index-1 ,offset);
+        return dp[index][target+offset] = add + sub;
     }
 }
